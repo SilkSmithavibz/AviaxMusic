19cf4d4a82cb66ba2ba008dbd7ac366e190dd943
Marwin040 <markusmarwin00@gmail.com>
2024-09-24 21:04:40 +0530

    Update start.py

--- AviaxMusic/plugins/bot/start.py
+++ AviaxMusic/plugins/bot/start.py
@@ -23,7 +23,6 @@ from AviaxMusic.utils.database import (
 )
 from AviaxMusic.utils.decorators.language import LanguageStart
 from AviaxMusic.utils.formatters import get_readable_time
-from AviaxMusic.utils.inline import private_panel, start_pannel
 
 
 loop = asyncio.get_running_loop()
