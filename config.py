8e631e737be9a7e9e4e664adf2ddeb9d610def7a
Marwin040 <markusmarwin00@gmail.com>
2024-09-25 13:38:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -55,7 +55,7 @@ SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", "409e31d3ddd64af08cfcc3b
 
 
 # Maximum limit for fetching playlist's track from youtube, spotify, apple links.
-PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", 25))
+PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", 500))
 
 
 # Telegram audio and video file size limit (in bytes)
