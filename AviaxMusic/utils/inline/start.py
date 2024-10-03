f0290abcf191dd93c61225cd88fcb64342e2fb04
Marwin040 <markusmarwin00@gmail.com>
2024-10-03 22:18:24 +0530

    Update start.py

--- AviaxMusic/utils/inline/start.py
+++ AviaxMusic/utils/inline/start.py
@@ -30,7 +30,7 @@ def private_panel(_):
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_6"], url=f"https://t.me/User_Dead_200"),
+            InlineKeyboardButton(text=_["S_B_6"], url=f"https://t.me/Infinity_heavenx"),
         ],
     ]
     return buttons
