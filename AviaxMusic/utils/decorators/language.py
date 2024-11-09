c8645f68e375163fdec0b618f7cb9db3d81b0680
Marwin040 <markusmarwin00@gmail.com>
2024-11-09 17:12:03 +0530

    Update language.py

--- AviaxMusic/utils/decorators/language.py
+++ AviaxMusic/utils/decorators/language.py
@@ -5,7 +5,6 @@ from strings import get_string
 
 def language(mystic):
     async def wrapper(_, message, **kwargs):
-        # Check if the bot is in maintenance mode
         if await is_maintenance() is False:
             if message.from_user.id not in SUDOERS:
                 return await message.reply_text(
@@ -18,11 +17,9 @@ def language(mystic):
             pass
 
         try:
-            # Get language for the chat
             language = await get_lang(message.chat.id)
             language = get_string(language)
         except:
-            # Default to English if no language is set
             language = get_string("en")
         return await mystic(_, message, language)
 
@@ -31,7 +28,6 @@ def language(mystic):
 
 def languageCB(mystic):
     async def wrapper(_, CallbackQuery, **kwargs):
-        # Check if the bot is in maintenance mode
         if await is_maintenance() is False:
             if CallbackQuery.from_user.id not in SUDOERS:
                 return await CallbackQuery.answer(
@@ -39,11 +35,9 @@ def languageCB(mystic):
                     show_alert=True,
                 )
         try:
-            # Get language for the chat
             language = await get_lang(CallbackQuery.message.chat.id)
             language = get_string(language)
         except:
-            # Default to English if no language is set
             language = get_string("en")
         return await mystic(_, CallbackQuery, language)
 
@@ -53,14 +47,10 @@ def languageCB(mystic):
 def LanguageStart(mystic):
     async def wrapper(_, message, **kwargs):
         try:
-            # Get language for the chat
             language = await get_lang(message.chat.id)
             language = get_string(language)
         except:
-            # Default to English if no language is set
             language = get_string("en")
-        
-        # Execute the wrapped function with the selected language
         return await mystic(_, message, language)
 
     return wrapper
