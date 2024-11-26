4e7371dd114b61d2231ee0399388c85197906277
SilkSmithavibz <Silksmithavibz@gmail.com>
2024-11-26 19:31:45 +0530

    Update database.py

--- AviaxMusic/utils/database.py
+++ AviaxMusic/utils/database.py
@@ -3,8 +3,8 @@ import asyncio
 from datetime import date
 from typing import Dict, List, Union
 
-from AviaxMusic import userbot
-from AviaxMusic.core.mongo import 
+from AviaxMusic import userbot 
+from AviaxMusic.core.mongo import mongodb
 
 authdb = mongodb.adminauth
 authuserdb = mongodb.authuser
